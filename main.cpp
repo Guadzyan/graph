3ed8185b7774d26bdb8528482bdee164e1ab68ad
Guadzyan <bachurin@yahoo.com>
2020-03-19 22:10:03 +0300

    v 0.01

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int ground = 500;
 class PLAYER { /* создаем класс PLAYER( нам нужно чтобы движения персонажа осуществлялись под действием  гравитации) */
 
 public:
-    float dx, dy; // скорость
+    float dx, dy, dz; // скорость
     FloatRect rect; //  здесь rect(x, y, widht, height)
     bool onGround; /*  onGround- переменная, которая показывает, находится ли персонаж на земле */
     Sprite sprite; // сюда будем загружать картинку
@@ -16,13 +16,14 @@ public:
         sprite.setTexture(image); // в конструктор класса загружаем картинку
         rect = FloatRect(0, 0, 40, 50); /* указываем первоначальные координаты x=0, y=0, ширина-40, высота-50 */
 
-        dx = dy = 0;
+        dx = dy = dz = 0;
         currentFrame = 0;
     }
 
     void update(float time)
     {
         rect.left += dx*time; // rect.left-есть координата х, перемещаем ее на dx*time
+        rect.left += dz*time; // dz переменная
 
         if (!onGround) dy = dy + 0.0005*time; /* если мы не на земле, то падаем с ускорением ( ускорение -0.005 умножаем на время получаем скорость) */
         rect.top += dy*time; // rect.top - есть координата у
@@ -35,16 +36,19 @@ public:
         if (currentFrame > 6) currentFrame -= 6; // всего у нас 6 кадров
         if (dx > 0) sprite.setTextureRect(IntRect(40 * int(currentFrame), 244, 40, 50)); /* меняем первую координату, то есть рисунок текстуры сдвигается каждый раз на 40( при движении направо- правая анимация */
         if (dx < 0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 244, -40, 50)); // при движении налево- зеркальная
+        if (currentFrame > 4) currentFrame -= 4; // всего у нас 4 кадра
+        if (dz > 0) sprite.setTextureRect(IntRect(36 * int(currentFrame),526,38,34));
+        if (dz < 0) sprite.setTextureRect(IntRect(36 * int(currentFrame) + 40,526,-38,34));
 
         sprite.setPosition(rect.left, rect.top); // выводим наш спрайт в позицию x, y
 
-        dx = 0;
+        dx = dz = 0;
     }
 };
 
 int main()
 {
-    RenderWindow window(VideoMode(600, 600), "SFMLworks"); // создаем окно 200 на 200 с именем SFMLworks */
+    RenderWindow window(VideoMode(600, 600), "SFMLworks"); // создаем окно 600 на 600 с именем SFMLworks */
 
                                                                                               Texture t;
     t.loadFromFile("/Users/igorbacurin/CLionProjects/graph/images/fang.png"); // загружаем картинку
@@ -76,6 +80,14 @@ int main()
         {
             p.dx = 0.1; // при нажатии направо- ускоряемся на 0.1
 
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down) && Keyboard::isKeyPressed(Keyboard::Right)) // если клавиша вправо нажата и клавиша пробел
+        {
+            p.dz = 0.1;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down) && Keyboard::isKeyPressed(Keyboard::Left)) // если клавиша влево нажата и клавиша пробел
+        {
+            p.dz = -0.1;
         }
         if (Keyboard::isKeyPressed(Keyboard::Up)) // вверх
         {
